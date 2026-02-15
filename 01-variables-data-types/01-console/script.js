93f524c83891913d54846f95362620531ab1389b
fareedah001 <ajibolafareedah02@gmail.com>
2026-02-15 23:22:35 +0100

    console

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,17 @@
+console.log(100);
+console.log('Hello World');
+console.log(20, "faridat", true);
+const x= 1000;
+console.log(x);
+console.error("alert");
+console.warn('Warning: Something went wrong!');
+console.table({name:"faridat", gmail: "fareedah02@gmail.com"})
+console.group("simple")
+console.log(x);
+console.error("alert");
+console.warn('Warning: Something went wrong!');
+console.groupEnd("simple");
+
+//Adding css to JS
+const style = "padding:10px; background-color:red; color:white";    
+console.log("%cHELLO WORLD", style);
