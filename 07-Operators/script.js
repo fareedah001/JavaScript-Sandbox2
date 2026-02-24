13a087ceb3b67e7bbc4743fa3f378bd23c84dfab
fareedah001 <ajibolafareedah02@gmail.com>
2026-02-24 23:44:42 +0100

    finished the string lesson

---
+++ 07-Operators/script.js
@@ -0,0 +1,40 @@
+// 1.Arithemetic Operators
+let x;
+x=10+10;
+x=2-3;
+x=10*2;
+x=10/2;
+x=10%2;
+// Concatenation Operator
+x="hello"+"world";
+
+//Exponent
+x=2**3;
+//Increment
+x=1;
+// x=x+1;
+x++;
+//Decrement
+// x=x-1;
+x--;
+
+//Assignment Operators
+x=10;
+x+=10;
+x-=10;
+x*=10;
+x/=10;
+x%=10;
+
+
+//3. Comparison Operators
+x= 2 ==2;
+x= 2 ===2;
+x= 2 !=2;
+x= 2 >2;
+x= 2 <2;
+x= 2 >=2;
+x= 2 <=2;
+
+
+console.log(x);
